7ea954b011d4084f8e3df9385a7122705fbbbf16
juldebar <juldebar@gmail.com>
2022-11-16 14:44:11 +0100

    improve query to exclude schemas and tables

--- SQL/data_dictionnary_tables.sql
+++ SQL/data_dictionnary_tables.sql
@@ -1,3 +1,7 @@
-SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND   schemaname != 'information_schema';
+SELECT schemaname,tablename FROM pg_catalog.pg_tables 
+WHERE schemaname != 'pg_catalog' 
+AND   schemaname != 'information_schema'
+ORDER BY schemaname,tablename ;
+
 -- SELECT * FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('information_schema', 'pg_catalog');
 
