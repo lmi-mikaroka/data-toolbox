25589061b90fcfd6ead92ad13dac2ec24b9b937a
herinomena <a.herinomena11@gmail.com>
2022-11-23 14:49:42 +0300

    ajout du requet list des vues materialise

---
+++ SQL/database_materialized_view.sql
@@ -0,0 +1,11 @@
+--list la liste des vues materialisées avec leur schema,le nom de la vue,proprietaire de la vue,
+--l'information sur le remplisage et sa definition
+
+SELECT schemaname AS table_schema,
+       matviewname AS table_name,
+       matviewowner AS table_owner,
+       ispopulated AS is_populated,
+       definition
+FROM pg_matviews
+ORDER BY table_schema,
+         table_name;
