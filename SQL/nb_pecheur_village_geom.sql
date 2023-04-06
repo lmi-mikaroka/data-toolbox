96f9b76c475aa0c60c1d6475e2835f87f8bcef52
Herinomena <95677559+Herinomena@users.noreply.github.com>
2023-04-06 16:03:27 +0300

    Update nb_pecheur_village_geom.sql

--- SQL/nb_pecheur_village_geom.sql
+++ SQL/nb_pecheur_village_geom.sql
@@ -1,13 +1,1 @@
-select
-	village.nom as nom_village,
-	village.the_geom ,
-	count(*)as nombre_de_pecheur
-from
-	pecheur
-inner join village on
-	pecheur.village = village.id
-group by
-	village.nom,
-	village.the_geom
-order by
-	nombre_de_pecheur desc
+SELECT * FROM public.nb_pecheur_village_geom
