656c49660b1eb832850cb663306d918cd8557325
herinomena <a.herinomena11@gmail.com>
2023-09-08 09:29:40 +0300

    add json file and R

---
+++ Json/exe_json.R
@@ -0,0 +1,16 @@
+#######################
+library(RPostgres)
+library(RPostgreSQL)
+library(DBI)
+library(geoflow)
+library(dotenv)
+
+#######################
+
+setwd(Sys.getenv("WD"))
+
+load_dot_env(file = "C:/Users/Herinomena/Desktop/exemple_env/.env")
+
+geoflow::initWorkflow("C:/Users/Herinomena/Desktop/exemple_env/Mikaroka_230804_env.json")
+
+geoflow::executeWorkflow("C:/Users/Herinomena/Desktop/exemple_env/Mikaroka_230804_env.json")
